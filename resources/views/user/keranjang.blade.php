4820d9475da908f8ebe37aa9df8dac4682bc661b
nan74k <nanta.pslove@gmail.com>
2023-02-02 07:21:22 +0700

    ii

--- resources/views/user/keranjang.blade.php
+++ resources/views/user/keranjang.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <div class="container my-8">
 
-    @error('overweight')
-    <div class="px-8 mb-4">
+    {{-- @error('overweight') --}}
+    <div class="w-full px-8 mb-4">
         <div id="alert-border-2" class="flex p-4 mb-4 text-red-700 bg-red-100 border-t-4 border-red-500 dark:text-red-400" role="alert">
             <svg class="flex-shrink-0 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
             <div class="ml-3 text-sm font-medium">
-                {{ $message }}
+                Berat
             </div>
             <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-red-100 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8"  data-dismiss-target="#alert-border-2" aria-label="Close">
             <span class="sr-only">Dismiss</span>
@@ -16,7 +16,7 @@
             </button>
         </div>
     </div>
-    @enderror
+    {{-- @enderror --}}
 
     <div class="w-full bg-white py-3 px-8 border shadow-sm mb-4">
         <div class="grid grid-cols-12 gap-2 text-gray-400 font-semibold">
