129f1b4ba92978da5e3ef1800f6de3a0e3c5f752
nan74k <nanta.pslove@gmail.com>
2023-02-20 11:10:46 +0700

    cetak

--- resources/views/admin/laporan/pesanan.blade.php
+++ resources/views/admin/laporan/pesanan.blade.php
@@ -8,7 +8,7 @@
         <span class="text-2xl">Laporan Pesanan</span>
       </div>
       <div>
-        <a href="{{route('admin.laporan.pesanan.print')}}" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Cetak</a>
+        <button onclick="cetak()" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Cetak</button>
       </div>
     </div>
     <div class="w-full border border-gray-600 rounded mt-8">
@@ -61,4 +61,13 @@
     </div>
 </div>
 
+@endsection
+
+@section('js')
+    <script>
+      function cetak() 
+      {
+        open("{{route('admin.laporan.pesanan.print')}}");
+      }
+    </script>
 @endsection
