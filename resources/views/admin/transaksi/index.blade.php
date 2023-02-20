dc7c4004db594c18d95278e045a2474fb1da5fc3
nan74k <nanta.pslove@gmail.com>
2023-02-20 10:45:56 +0700

    tgl-transaksi

--- resources/views/admin/transaksi/index.blade.php
+++ resources/views/admin/transaksi/index.blade.php
@@ -12,8 +12,8 @@
             <tr class="bg-gray-50">
               <th class="px-4 py-4 font-medium text-left text-gray-900">No</th>
               <th class="px-4 py-4 font-medium text-left text-gray-900 whitespace-nowrap">Nama Pemesan</th>
-              <th class="px-4 py-4 font-medium text-left text-gray-900 whitespace-nowrap">Jumlah</th>
               <th class="px-4 py-4 font-medium text-left text-gray-900 whitespace-nowrap">Total Pembayaran</th>
+              <th class="px-4 py-4 font-medium text-left text-gray-900 whitespace-nowrap">Tanggal</th>
               <th class="px-4 py-4 font-medium text-left text-gray-900 whitespace-nowrap">Status Pembayaran</th>
             </tr>
           </thead>
@@ -27,8 +27,8 @@
                 <tr>
                   <td class="px-4 py-5 font-medium text-gray-900">{{ $no }}</td>
                   <td class="px-4 py-5 text-gray-700 whitespace-nowrap">{{ $item->pesanan->user->nama_depan }}</td>
-                  <td class="px-4 py-5 text-gray-700 whitespace-nowrap">{{ $item->pesanan->jumlah }}</td>
                   <td class="px-4 py-5 text-gray-700 whitespace-nowrap">Rp{{ number_format($item->total, 0, '', '.') }}</td>
+                  <td class="px-4 py-5 text-gray-700 whitespace-nowrap">{{ Carbon\Carbon::parse($item->updated_at)->format('d M Y') }}</td>
                   <td class="px-4 py-5 text-gray-700 whitespace-nowrap">{{ strtoupper($item->status) }}</td>
                 </tr>
                 @php
