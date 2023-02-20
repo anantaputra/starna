953d34f6b6b12176250ed14125a52c033e24754d
nan74k <nanta.pslove@gmail.com>
2023-02-20 11:36:03 +0700

    aa

--- app/Http/Controllers/Admin/AdminTransaksiController.php
+++ app/Http/Controllers/Admin/AdminTransaksiController.php
@@ -19,7 +19,6 @@ class AdminTransaksiController extends Controller
     {
         $transaksi = Transaksi::whereBetween('created_at', [$request->mulai, $request->akhir])
                     ->orderBy('updated_at', 'DESC')
-                    ->get()
                     ->paginate(10);
 
             if (isset($transaksi)) {
